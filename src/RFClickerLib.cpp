5e80e00549571c3555d28b35a624806085128755
Patrick Gillan <pgillan@minorimpact.com>
2021-03-16 17:15:46 -0400

    Removed almost everything while I'm rewriting for the new hardware. WIP.

--- src/RFClickerLib.cpp
+++ src/RFClickerLib.cpp
@@ -1,96 +1,36 @@
+/*
 #include <RFClickerLib.h>
 
-//BLEDevice clicker;
+BLEService buttonService("1101");
+BLEByteCharacteristic buttonChar("2101", BLERead | BLEWrite);
 
-BLEDevice clicker;
-char ble_history[HISTORY_LENGTH*2];
-char temp_ble[HISTORY_LENGTH*2];
-uint8_t ble_id = 0;
-uint32_t last_scan = 0;
-
-void RFClickerSetup(char *history) {
+void RFClickerSetup() { //char *history) {
   BLE.begin();
   BLE.scanForName("RFClicker");
-  last_scan = millis();
-  arrayFill(0, history, HISTORY_LENGTH);
+  //arrayFill(0, history, HISTORY_LENGTH);
+
+  BLE.setDeviceName("DEVICENAME");
+  BLE.setLocalName("LOCALNAME");
+  BLE.setAdvertisedService(buttonService);
+  buttonService.addCharacteristic(buttonChar);
+  BLE.addService(buttonService);
+  BLE.setEventHandler(BLEConnected, onConnect);
+  BLE.setEventHandler(BLEDisconnected, onDisconnect);
+  buttonChar.setEventHandler(BLEWritten, RFClickerButtonClick);
+  buttonChar.setValue(0);
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
 }
 
-void getClickerHistory(char* history) {
-  uint32_t now = millis();
-  //Serial.print("free memory:");
-  //Serial.println(freeMemory());
-  //Serial.print("now:");
-  //Serial.println(now);
-  //Serial.println("Zeroing out array.");
-  arrayFill(0, history, HISTORY_LENGTH);
-  //Serial.println("Zeroed out array.");
-  if (!clicker.connected()) {
-    //Serial.println("Clicker is not connected.");
-    //Serial.print("last scan:");
-    //Serial.println(last_scan);
-    if (last_scan && now > last_scan + 60000) {
-        //Serial.println("stopping old scan");
-        BLE.stopScan();
-        last_scan = 0;
-        return;
-    }
-    else if (last_scan == 0) {
-      //Serial.println("starting new scan");
-      BLE.scanForName("RFClicker");
-      last_scan = now;
-      return;
-    }
-
-    clicker = BLE.available();
-    if (clicker) {
-      Serial.print("Found ");
-      Serial.print(clicker.address());
-      Serial.print(" '");
-      Serial.print(clicker.localName());
-      Serial.print("' ");
-      Serial.print(clicker.advertisedServiceUuid());
-      Serial.println();
-
-      if (clicker.localName() == "RFClicker") {
-        //Serial.println("found clicker");
-        BLE.stopScan();
-        last_scan = 0;
-        bleConn(clicker);
-      }
-    }
-    else {
-      //Serial.println("  done -- nothing.");
-      return;
-    }
-  }
-
-  if (!clicker.connected()) {
-    //Serial.println("Clicker is *still* not connected.");
-    return;
-  }
-
-  //Serial.println("Doing stuff");
-  uint8_t ble_pos = 0;
-  getBLEValue(clicker, temp_ble, HISTORY_LENGTH*2);
-  if (arrayCompare(temp_ble, ble_history, HISTORY_LENGTH*2)) {
-    return;
-  }
-  arrayCopy(temp_ble, ble_history, HISTORY_LENGTH*2);
-  for (uint8_t i = 1; i <= HISTORY_LENGTH; i++) {
-    uint8_t id = ble_history[(i-1)*2];
-    if (id == ble_id) {
-      ble_pos = i;
-      break;
-    }
-  }
-
-  if (ble_pos >= HISTORY_LENGTH) {
-      return;
-  }
-  for (uint8_t i = ble_pos + 1; i <= HISTORY_LENGTH; i++) {
-    uint8_t id = ble_history[(i-1)*2];
-    uint8_t ble_buttons = ble_history[((i-1)*2)+1];
-    arrayPush(ble_buttons, history, HISTORY_LENGTH);
-    ble_id = id;  
-  }
+void onConnect(BLEDevice central) {
+  Serial.print("connected to ");
+  Serial.println(central.address());
+  digitalWrite(LED_BUILTIN, HIGH);
 }
+
+void onDisconnect(BLEDevice central) {
+  Serial.print("disconnected from ");
+  Serial.println(central.address());
+  digitalWrite(LED_BUILTIN, LOW);
+}
+*/
--- src/RFClickerLib.h
+++ src/RFClickerLib.h
@@ -1,21 +1,22 @@
-#pragma once
 /* 
  * Includes
  */
-
-#define BLUETOOTH
-
-#include <PGardLib.h>
+//#include <PGardLib.h>
+//#include <ArduinoBLE.h>
 
 /*
  * Variables & Data Structures
  */
 
 #define HISTORY_LENGTH  10 
-#define BUTTON1(x)      x & 1 << 0
-#define BUTTON2(x)      x & 1 << 1
-#define BUTTON3(x)      x & 1 << 2
-#define BUTTON4(x)      x & 1 << 3
+#define BUTTON1         1
+#define BUTTON2         2
+#define BUTTON3         4
+#define BUTTON4         8
+
+#define ADDBUTTON(x,y)  x |= y
+#define CLRBUTTON(x,y)  x &= ~y
+#define TSTBUTTON(x,y)  x & y
 
 /*
 BLEDevice clicker;
@@ -24,10 +25,20 @@ char temp_ble[HISTORY_LENGTH*2];
 uint32_t last_ble = 0;
 uint8_t ble_id = 0;
 */
+//BLEService buttonService;
+//BLECharacteristic buttonChar;
+
 
 /*
  * Functions
  */
-void getClickerHistory(char* history);
-void RFClickerSetup(char *history);
+//void getClickerHistory(char* history);
+/*
+void RFClickerSetup();
+void onConnect(BLEDevice central);
+void onDisconnect(BLEDevice central);
+extern void RFClickerButtonClick(BLEDevice central, BLECharacteristic characteristic);
+*/
+//BLEService buttonService("1101");
+//BLEByteCharacteristic buttonChar("2101", BLERead | BLEWrite);
 
